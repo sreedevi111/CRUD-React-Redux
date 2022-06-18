5794ab8fc743bdcdc2a9c4eebdc08e7b63d6022d
sreedevi111 <sreedevisobhana@gmail.com>
2022-06-18 11:40:52 +0530

    reducer

--- crud/src/features/postSlice.js
+++ crud/src/features/postSlice.js
@@ -1,34 +1,92 @@
 // reducer
 
-import {  createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 //createAsyncThunk  to deal with API
 
-export const getPosts = createAsyncThunk("posts/getPosts", async() =>{
-    return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+export const getPosts = createAsyncThunk("posts/getPosts", async () => {
+  return fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
     res.json()
-    );
+  );
+});
 
-})
+export const createPosts = createAsyncThunk("posts/createPosts", async (body) => {
+  
+  return fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "post",
+    
+    body: JSON.stringify(body),
+    
+  }).then((res) => {res.json();console.log(body)}).then((data)=> data);
+});
 
-const postSlice = createSlice(({
-    name: "posts",
-    initialState:{
-        posts: [],
-        loading: false
+export const editPosts = createAsyncThunk("posts/editPosts", async (body,id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${body.id}`, {
+    method: "put",
+    body: body,
+  }).then((res) => res.json()).then((data)=> data);
+});
+
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    postCreated: {},
+    loading: false,
+  },
+  extraReducers: {
+    [getPosts.pending]: (state, action) => {
+      state.loading = true;
     },
-    extraReducers:{
-        [getPosts.pending] : (state, action) =>{
-            state.loading = true;
-        },
-        [getPosts.fulfilled] : (state, action) =>{
-            state.loading = false;
-            state.posts = action.payload
-        },
-        [getPosts.rejected]: (state, action)=>{
-            state.loading = false
-        },
-    }
-}))
+    [getPosts.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.posts = action.payload;
+    },
+    [getPosts.rejected]: (state, action) => {
+      state.loading = false;
+    },
 
-export default postSlice.reducer
+    addPost: (state, action) => {
+      state = state.push(action.payload);
+    },
+
+    [createPosts.fulfilled]: (state, action) => {
+        state.loading = false;
+        console.log("state:::",state)
+        console.log("action:::",action)
+        state.postCreated = action.payload
+      },
+      [createPosts.pending]: (state, action) => {
+        state.loading = true;
+      },
+      [createPosts.rejected]: (state, action) => {
+        state.loading = false;
+      },
+
+      [editPosts.fulfilled]: (state, action) => {
+        state.loading = false;
+        console.log("state:::",state)
+        console.log("action:::",action)
+        state.postCreated = action.payload
+      },
+      [editPosts.pending]: (state, action) => {
+        state.loading = true;
+      },
+      [editPosts.rejected]: (state, action) => {
+        state.loading = false;
+      },
+
+
+    // updatePosts: (state, action) => {
+    //   state.value.map((user) => {
+    //     if (user.id === action.payload.id) {
+    //       user.username = action.payload.username;
+    //     }
+    //     return user;
+    //   });
+    // },
+  },
+});
+
+export default postSlice.reducer;
